6a03adce14dac4c2215203e7a8906d659e90aec0
Roberto Albergo <ralbergo7@gmail.com>
2025-05-06 16:20:42 +0200

    es

--- src/ContoAllaRovescia.java
+++ src/ContoAllaRovescia.java
@@ -1,10 +1,14 @@
 package esercizio ;
 
 import java.util.Scanner;
+
 public class ContoAllaRovescia {
+
     public static void main (String[] args){
+
         Scanner scanner= new Scanner(System.in);
         System.out.print("Inserisci un numero intero: ");
+        
         int numero = scanner.nextInt();
 
         // Conto alla rovescia
